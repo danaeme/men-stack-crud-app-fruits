1190970cddb172b2eb7a93d8a39c4b347ff463de
David Anaeme <davidcanaeme@gmail.com>
2024-07-25 12:59:47 -0600

    Install mongoose and dotenv, copy connection string,  create fruit model

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
+//Connect to MongoDB 
+mongoose.connect(process.env.MONGODB_URI)
+// Log connection status to terminal on start 
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MONGODB ${mongoose.connection.name}`)
+})
+
+//Import the Fruit model
+const Fruit = require('./models/fruit.js')
+
 // GET '/'
 app.get('/', async (req, res) => {
-    res.send('Hello')
+    res.render('index.ejs')
 })
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
+
