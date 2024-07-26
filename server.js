a807e00a17331c7ba9e4d7bf47c8933aef95a462
David Anaeme <davidcanaeme@gmail.com>
2024-07-26 12:14:54 -0600

    Install morgan and method-override

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -18,6 +20,8 @@ const e = require('express')
 
 //middleware
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
 
 // GET '/'
 app.get('/', async (req, res) => {
@@ -28,7 +32,7 @@ app.get('/', async (req, res) => {
 app.get('/fruits', async (req, res) => {
     const allFruits = await Fruit.find()
     console.log(allFruits)
-    res.render("fruits/index.ejs", { fruits: allFruits })
+    res.render('fruits/index.ejs', { fruits: allFruits })
 })
 
 // GET '/fruits/new
@@ -48,9 +52,41 @@ app.post("/fruits", async (req, res) => {
     } else {
       req.body.isReadyToEat = false;
     }
-    await Fruit.create(req.body);
     res.redirect("/fruits"); // redirect to index fruits
+    await Fruit.create(req.body);
   });
+
+  //DELETE 
+  app.delete('/fruits/:fruitId', async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId)
+    //redirect after deleting the object:
+    res.redirect('/fruits')
+  })
+
+// GET localhost:3000/fruits/:fruitId/edit
+// app.get("/fruits/:fruitId/edit", async (req, res) => {
+//   const foundFruit = await Fruit.findById(req.params.fruitId);
+//   console.log(foundFruit);
+//   res.send(`This is the edit route for ${foundFruit.name}`);
+// });
+
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
